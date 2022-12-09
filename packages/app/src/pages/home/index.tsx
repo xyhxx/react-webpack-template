0e75139101f162298a921e144da5d71e7e97f6a0
xyh <603331487@qq.com>
2022-12-09 17:49:23 +0800

    change class-hook to classnames

--- packages/app/src/pages/home/index.tsx
+++ packages/app/src/pages/home/index.tsx
@@ -2,23 +2,22 @@ import icon from '@assets/images/logo.svg';
 
 import css from './index.module.scss';
 
-import {useClassNames} from 'react-hook-classnames';
+import cla from 'classnames';
 import {FC} from 'react';
 import {useCount} from '@stores';
 import shallow from 'zustand/shallow';
 
 const Home: FC = function() {
-  const [style, cla] = useClassNames({styleSheet: css, camelTransition: '-'});
   const {count, inc, dec} = useCount(state => state, shallow);
 
   return (
     <>
-      <img src={icon} className={style.icon} />
-      <h1 className={cla([style.title, style.name, {[style.titleRed]: count >= 5}])}>React</h1>
-      <h2 className={style.title} data-testid='title'>
+      <img src={icon} className={css.icon} />
+      <h1 className={cla([css.title, css.name, {[css['title-red']]: count >= 5}])}>React</h1>
+      <h2 className={css.title} data-testid='title'>
         count is {count}
       </h2>
-      <div className={style.btnGroup}>
+      <div className={css['btn-group']}>
         <button onClick={inc} data-testid='increment'>
           increment
         </button>
