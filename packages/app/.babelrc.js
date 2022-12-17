10d08d8ea95da0b88b20c82ceb49385b73448ed3
xyhxx <603331487@qq.com>
2022-12-17 15:46:04 +0800

    fix: remove babel cache

--- packages/app/.babelrc.js
+++ packages/app/.babelrc.js
@@ -35,5 +35,6 @@ export default {
         'properties': ['data-testid'],
       },
     ],
+    isDev && 'react-refresh/babel',
   ].filter(Boolean),
 }
--- packages/webpack/config/babel.js
+++ packages/webpack/config/babel.js
@@ -3,9 +3,7 @@ const {
   enableThreadLoader,
 } = require('./constants.js');
 
-const {resolve} = require('path');
-
-const {srcPath, rootPath} = require('./paths.js');
+const {srcPath} = require('./paths.js');
 
 module.exports = {
   test: /\.(js|jsx|ts|tsx|mjs)$/,
@@ -15,13 +13,7 @@ module.exports = {
     {
       loader: require.resolve('babel-loader'),
       options: {
-        plugins: [
-          !isProduction
-          && require.resolve('react-refresh/babel'),
-        ].filter(Boolean),
         compact: isProduction,
-        cacheDirectory: resolve(rootPath, '.temp-cache/babel-cache'),
-        cacheCompression: false,
       },
     },
   ].filter(Boolean),
