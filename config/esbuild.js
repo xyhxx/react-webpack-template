3e6e4313ce4710233147ee7007a2114605fcc72c
xyhxx <603331487@qq.com>
2022-12-18 13:39:20 +0800

    fix: esbuild-loader include

--- config/esbuild.js
+++ config/esbuild.js
@@ -1,9 +1,11 @@
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+import {srcPath} from './paths.js';
 
 export default function esbuild(isTs) {
   return {
     test: !isTs ? /\.(js|jsx|mjs)$/ : /\.tsx?$/,
     loader: 'esbuild-loader',
+    include: srcPath,
     options: {
       loader: isTs ? 'tsx' : 'jsx',
       target: browserslistToEsbuild(),
