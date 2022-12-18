a5d858b06e32a6fb5be9a2fc8ff23c68ef95e09a
xyhxx <603331487@qq.com>
2022-12-18 14:06:33 +0800

    fix: esbuild-loader include

--- packages/webpack/config/esbuild.js
+++ packages/webpack/config/esbuild.js
@@ -1,9 +1,11 @@
 const browserslistToEsbuild = require('browserslist-to-esbuild');
+const {srcPath} = require('./paths.js');
 
 function esbuild(isTs) {
   return {
     test: !isTs ? /\.(js|jsx|mjs)$/ : /\.tsx?$/,
     loader: require.resolve('esbuild-loader'),
+    include: srcPath,
     options: {
       loader: isTs ? 'tsx' : 'jsx',
       target: browserslistToEsbuild(),
