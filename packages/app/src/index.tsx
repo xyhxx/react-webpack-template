90430c279162924eee74d7ff644e74b13941cb7c
xyhxx <603331487@qq.com>
2023-02-15 21:11:10 +0800

    feat: add web-vitals

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -6,6 +6,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import {StrictMode} from 'react';
+import {reportWebVitals} from '@utils';
 
 const queryClient = new QueryClient();
 
@@ -21,3 +22,5 @@ root.render(
     </QueryClientProvider>
   </StrictMode>,
 );
+
+process.env.NODE_ENV === 'development' && reportWebVitals();
---
+++ packages/app/src/utils/reportWebVitals.ts
@@ -0,0 +1,20 @@
+
+const {log} = console;
+
+export function reportWebVitals() {
+  import('web-vitals')
+    .then(function({onCLS, onFID, onFCP, onLCP, onTTFB, onINP}) {
+      // Time to First Byte
+      onCLS(log);
+      // First Contentful Paint
+      onFID(log);
+      // Largest Contentful Paint
+      onFCP(log);
+      // Cumulative Layout Shift
+      onLCP(log);
+      // First Input Delay
+      onTTFB(log);
+      // Interaction to Next Paint
+      onINP(log);
+    });
+}
