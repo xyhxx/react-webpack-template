89de5c56f9b42dc58f233bc0f9a131bd2938bd13
xyh <603331487@qq.com>
2023-02-15 15:39:08 +0800

    chore: webpackDevSercer add headers

--- packages/webpack/config/devServer.js
+++ packages/webpack/config/devServer.js
@@ -1,6 +1,11 @@
 const {address} = require('ip');
 
 module.exports = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Headers': '*',
+  },
   open: !process.env.IS_E2E,
   host: address(),
   port: Number(process.env.HOST),
