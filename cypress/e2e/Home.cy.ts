934f1bc1a94dbad6fc1edf67056d6be1a9476695
xyhxx <603331487@qq.com>
2023-04-17 21:09:16 +0800

    chore: modify getTestId to getByTestId

--- cypress/e2e/Home.cy.ts
+++ cypress/e2e/Home.cy.ts
@@ -2,15 +2,15 @@ describe('Home page', function () {
   it('Home page e2e', function () {
     cy.visit('/');
 
-    cy.getTestId('title').should('contain.text', 'count is 0');
+    cy.getByTestId('title').should('contain.text', 'count is 0');
 
-    cy.getTestId('increment').click();
-    cy.getTestId('title').should('contain.text', 'count is 2');
+    cy.getByTestId('increment').click();
+    cy.getByTestId('title').should('contain.text', 'count is 2');
 
-    cy.getTestId('increment').click();
-    cy.getTestId('title').should('contain.text', 'count is 4');
+    cy.getByTestId('increment').click();
+    cy.getByTestId('title').should('contain.text', 'count is 4');
 
-    cy.getTestId('reduce').click();
-    cy.getTestId('title').should('contain.text', 'count is 2');
+    cy.getByTestId('reduce').click();
+    cy.getByTestId('title').should('contain.text', 'count is 2');
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -7,6 +7,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
-Cypress.Commands.add('getTestId' as any, function (value) {
+Cypress.Commands.add('getByTestId' as any, function (value) {
   return cy.get(`[data-testid="${value}"]`);
 });
