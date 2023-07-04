76f1c64499ffd4ded3f408c86a9021c85c657593
xyhxx <603331487@qq.com>
2023-07-04 21:51:38 +0800

    update: remove react-query-devtools

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,7 +2,6 @@ import '@styles/index.css';
 import {createRoot} from 'react-dom/client';
 import {RouterProvider} from 'react-router-dom';
 import {QueryClientProvider} from '@tanstack/react-query';
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import {StrictMode} from 'react';
 import {QUERY_CLIENT} from '@utils';
 import routes from '@routes';
@@ -13,7 +12,6 @@ root.render(
   <StrictMode>
     <QueryClientProvider client={QUERY_CLIENT}>
       <RouterProvider router={routes} />
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </StrictMode>,
 );
